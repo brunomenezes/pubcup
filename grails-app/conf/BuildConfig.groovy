9df3a5323a11412d499404d34aab60cb70d6ff2b
fabio.oshiro <fabio.oshiro@gmail.com>
2013-06-12 00:37:29 -0300

    target local

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,6 +6,9 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+// setting work dir to target subdirectory (instead of user home directory)
+grails.work.dir = 'target'
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
